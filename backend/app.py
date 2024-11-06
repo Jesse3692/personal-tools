f3ec401174d01ea8626514479a3bb37f6ede97f9
Jesse3692 <lcf.zhangxing@hotmail.com>
2024-11-06 12:24:27 +0800

    feat: ping

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+
+@app.route("/ping")
+def ping_pong():
+    return jsonify("pong!")
 
 
 if __name__ == "__main__":
